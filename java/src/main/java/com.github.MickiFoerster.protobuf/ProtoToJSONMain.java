344544b1314120475baa7fe416958597c054c673
micki <github@com-science.de>
2019-12-12 23:10:15 +0100

    Make it the java way

---
+++ java/src/main/java/com.github.MickiFoerster.protobuf/ProtoToJSONMain.java
@@ -0,0 +1,38 @@
+package com.github.MickiFoerster.protobuf;
+
+import com.example.Simple.SimpleMessage;
+import com.google.protobuf.*;
+import com.google.protobuf.util.JsonFormat;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ProtoToJSONMain {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    SimpleMessage.Builder builder = SimpleMessage.newBuilder();
+
+    // simple field
+    builder.setId(42);
+    builder.setIsSimple(true);
+    builder.setName("This is a simpel message");
+
+    builder.setId(23).setIsSimple(true).setName("This is another message");
+
+    // repeated field
+    builder.addSampleList(1).addSampleList(2).addSampleList(3).addSampleList(4);
+
+    builder.addAllSampleList(Arrays.asList(4, 5, 6));
+
+    // Print this as a JSON
+    try {
+      String jsonString = JsonFormat.printer().print(builder);
+      System.out.println(jsonString);
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
+    }
+  }
+}
